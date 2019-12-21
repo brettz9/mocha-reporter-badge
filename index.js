4ae77a226e898e2d3830f2ff2e08b1772f5bfe3e
Brett Zamir <brettz9@yahoo.com>
2019-12-21 09:11:28 +0800

    - Switch from shields-lightweight to more recently maintained gh-badges - Enhancement: Allow label color to be set

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const util = require('util');
 const writeFile = util.promisify(fs.writeFile);
 
-const shields = require('shields-lightweight');
+const { BadgeFactory } = require('gh-badges');
+const bf = new BadgeFactory()
 
 module.exports = BadgeReporter;
 
@@ -23,19 +24,27 @@ function BadgeReporter(runner, options) {
 
   runner.on('end', function () {
     const {
-      badge_subject, badge_ok_color, badge_ko_color, badge_style, badge_path
+      badge_subject, badge_ok_color, badge_ko_color, badge_label_color,
+      badge_style, badge_path
     } = (options && options.reporterOptions) || {};
 
     const subject = badge_subject || process.env.MOCHA_BADGE_SUBJECT || 'tests';
     const okColor = badge_ok_color || process.env.MOCHA_BADGE_OK_COLOR || 'brightgreen';
     const koColor = badge_ko_color || process.env.MOCHA_BADGE_KO_COLOR || 'red';
+    const labelColor = badge_label_color || process.env.MOCHA_BADGE_LABEL_COLOR; // gray is default
     const style = badge_style || process.env.MOCHA_BADGE_STYLE;
 
     const color = (failures > 0) ? koColor : okColor;
     const status = passes + '/' + (passes + failures);
 
-    const svg = shields.svg(subject, status, color, style);
-
+    const format = {
+      text: [subject, status],
+      format: 'svg', // or `json`
+      color,
+      labelColor,
+      template: style
+    };
+    const svg = bf.create(format);
     if (badge_path) {
       writeFile(badge_path, svg);
       return;
